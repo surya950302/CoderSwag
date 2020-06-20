3269c6dfa93de65b5488f799d3fec8efdc7cec94
surya <surya950302@gmail.com>
2020-06-20 19:01:31 -0400

    List view commit 2 with custom adapter

--- app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.surya.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.surya.coderswag.Adapters.CategoryAdapter
 import com.surya.coderswag.Model.Category
 import com.surya.coderswag.R
 import com.surya.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
     }
--- app/src/main/java/com/surya/coderswag/Services/DataService.kt
+++ app/src/main/java/com/surya/coderswag/Services/DataService.kt
@@ -8,7 +8,7 @@ object DataService{
         Category("SHIRTS","shirtimage"),
         Category("HOODIES","hoodieimage"),
         Category("HATS","hatimage"),
-        Category("DIGITAL","digitalgoosimage")
+        Category("DIGITAL","digitalgoodsimage")
     )
 
     val hats = listOf(
