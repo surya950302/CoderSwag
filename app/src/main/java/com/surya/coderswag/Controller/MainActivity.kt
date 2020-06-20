5d3a49d76ed7a517394b21209296492271241746
surya <surya950302@gmail.com>
2020-06-20 17:48:55 -0400

    List view commit 1

--- app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
@@ -2,11 +2,22 @@ package com.surya.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.surya.coderswag.Model.Category
 import com.surya.coderswag.R
+import com.surya.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, DataService.categories)
+        categoryListView.adapter = adapter
+
     }
 }
