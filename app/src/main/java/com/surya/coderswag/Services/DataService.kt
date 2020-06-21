4008690518a9920811397235d89753dba482522d
surya <surya950302@gmail.com>
2020-06-21 02:55:44 -0400

    Recycler view commit 1 for product

--- app/src/main/java/com/surya/coderswag/Services/DataService.kt
+++ app/src/main/java/com/surya/coderswag/Services/DataService.kt
@@ -12,24 +12,43 @@ object DataService{
     )
 
     val hats = listOf(
-        Product("Deveslopes Graphis Beanie","$20", "hat01"),
-        Product("Deveslopes Graphis Hat balck","$10", "hat02"),
-        Product("Deveslopes Graphis Hat White","$16", "hat03"),
-        Product("Deveslopes Graphis Snapback","$7.99", "hat04")
+        Product("Deveslopes Graphis Beanie","$20", "hat1"),
+        Product("Deveslopes Graphis Hat balck","$10", "hat2"),
+        Product("Deveslopes Graphis Hat White","$16", "hat3"),
+        Product("Deveslopes Graphis Snapback","$7.99", "hat4")
     )
     val hoodies = listOf(
-        Product("Deveslopes Hoodie Grey","$10", "hoodie01"),
-        Product("Deveslopes Hoodie Red","$32.49", "hoodie02"),
-        Product("Deveslopes Grey Hoodie","$19.99", "hoodie03"),
-        Product("Deveslopes Graphis Black Hoodie","$14.99", "hoodie04")
+        Product("Deveslopes Hoodie Grey","$10", "hoodie1"),
+        Product("Deveslopes Hoodie Red","$32.49", "hoodie2"),
+        Product("Deveslopes Grey Hoodie","$19.99", "hoodie3"),
+        Product("Deveslopes Graphis Black Hoodie","$14.99", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Deveslopes Shirt Black","$29.49", "shirt01"),
-        Product("Deveslopes Badge light Grey","$32.49", "shirt02"),
-        Product("Deveslopes logo Shirt Red","$29.99", "shirt03"),
-        Product("Deveslopes Hustle","$14.99", "shirt04"),
-        Product("Kikflip Studios","$34.99", "shirt05")
+        Product("Deveslopes Shirt Black","$29.49", "shirt1"),
+        Product("Deveslopes Badge light Grey","$32.49", "shirt2"),
+        Product("Deveslopes logo Shirt Red","$29.99", "shirt3"),
+        Product("Deveslopes Hustle","$14.99", "shirt4"),
+        Product("Kikflip Studios","$34.99", "shirt5")
     )
+
+    val digitalGoods = listOf<Product>( )
+
+    fun getProducts(category : String) : List<Product>{
+        var list = listOf<Product>()
+        if(category == "HATS"){
+            list = hats
+        }
+        else if(category == "SHIRTS"){
+            list = shirts
+        }
+        else if(category == "HOODIES"){
+            list = hoodies
+        }
+        else{
+            list = digitalGoods
+        }
+        return list
+    }
 
 }
