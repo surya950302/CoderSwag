4f94d84d4f63f0e5161a36ac658003a5d7f302aa
surya <surya950302@gmail.com>
2020-06-21 00:25:25 -0400

    List view commit 3 with custom adapter

--- app/src/main/java/com/surya/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/surya/coderswag/Adapters/CategoryAdapter.kt
@@ -10,6 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.surya.coderswag.Model.Category
 import com.surya.coderswag.R
+import org.w3c.dom.Text
 
 class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
 
@@ -18,15 +19,28 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+        val holder: ViewHolder
+
+        if(convertView == null ){
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        }
+        else{
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
 
         val category = categories[position]
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
 
-        categoryImage.setImageResource(resourceId)
-        categoryName.text = category.title
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
 
         return categoryView
     }
@@ -41,5 +55,10 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getCount(): Int {
         return categories.count()
+    }
+
+    private class ViewHolder{
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
--- app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/surya/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.surya.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.surya.coderswag.Adapters.CategoryAdapter
 import com.surya.coderswag.Model.Category
 import com.surya.coderswag.R
@@ -19,6 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        /*categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "you clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
+
+        }*/
 
     }
 }
