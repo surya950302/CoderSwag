a7143c46647b4bd101ce1a07f147c3e836659d67
surya <surya950302@gmail.com>
2020-06-21 03:39:22 -0400

    Recycler view commit 1 for product

--- app/src/main/java/com/surya/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/surya/coderswag/Controller/ProductsActivity.kt
@@ -1,18 +1,49 @@
 package com.surya.coderswag.Controller
 
+import android.content.res.Configuration
+import android.media.VolumeShaper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.surya.coderswag.Adapters.ProductAdapter
 import com.surya.coderswag.R
+import com.surya.coderswag.Services.DataService
 import com.surya.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var categoryType : String
+    lateinit var adapter : ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
-        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        Toast.makeText(this, "you clicked on $categoryType as category type", Toast.LENGTH_SHORT).show()
+        categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        //Toast.makeText(this, "you clicked on $categoryType as category type", Toast.LENGTH_SHORT).show()
+
+        adapter = ProductAdapter(this,DataService.getProducts(categoryType))
+        productListView.adapter = adapter
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+        else{
+            spanCount = 2
+        }
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }else if(screenSize > 720 && orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 4
+        }
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productListView.layoutManager = layoutManager
+
     }
 }
--- app/src/main/java/com/surya/coderswag/Services/DataService.kt
+++ app/src/main/java/com/surya/coderswag/Services/DataService.kt
@@ -8,10 +8,22 @@ object DataService{
         Category("SHIRTS","shirtimage"),
         Category("HOODIES","hoodieimage"),
         Category("HATS","hatimage"),
+        Category("DIGITAL","digitalgoodsimage"),
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("DIGITAL","digitalgoodsimage")
     )
 
     val hats = listOf(
+        Product("Deveslopes Graphis Beanie","$20", "hat1"),
+        Product("Deveslopes Graphis Hat balck","$10", "hat2"),
+        Product("Deveslopes Graphis Hat White","$16", "hat3"),
+        Product("Deveslopes Graphis Snapback","$7.99", "hat4"),
+        Product("Deveslopes Graphis Beanie","$20", "hat1"),
+        Product("Deveslopes Graphis Hat balck","$10", "hat2"),
+        Product("Deveslopes Graphis Hat White","$16", "hat3"),
+        Product("Deveslopes Graphis Snapback","$7.99", "hat4"),
         Product("Deveslopes Graphis Beanie","$20", "hat1"),
         Product("Deveslopes Graphis Hat balck","$10", "hat2"),
         Product("Deveslopes Graphis Hat White","$16", "hat3"),
@@ -21,10 +33,29 @@ object DataService{
         Product("Deveslopes Hoodie Grey","$10", "hoodie1"),
         Product("Deveslopes Hoodie Red","$32.49", "hoodie2"),
         Product("Deveslopes Grey Hoodie","$19.99", "hoodie3"),
+        Product("Deveslopes Graphis Black Hoodie","$14.99", "hoodie4"),
+        Product("Deveslopes Hoodie Grey","$10", "hoodie1"),
+        Product("Deveslopes Hoodie Red","$32.49", "hoodie2"),
+        Product("Deveslopes Grey Hoodie","$19.99", "hoodie3"),
+        Product("Deveslopes Graphis Black Hoodie","$14.99", "hoodie4"),
+        Product("Deveslopes Hoodie Grey","$10", "hoodie1"),
+        Product("Deveslopes Hoodie Red","$32.49", "hoodie2"),
+        Product("Deveslopes Grey Hoodie","$19.99", "hoodie3"),
         Product("Deveslopes Graphis Black Hoodie","$14.99", "hoodie4")
+
     )
 
     val shirts = listOf(
+        Product("Deveslopes Shirt Black","$29.49", "shirt1"),
+        Product("Deveslopes Badge light Grey","$32.49", "shirt2"),
+        Product("Deveslopes logo Shirt Red","$29.99", "shirt3"),
+        Product("Deveslopes Hustle","$14.99", "shirt4"),
+        Product("Kikflip Studios","$34.99", "shirt5"),
+        Product("Deveslopes Shirt Black","$29.49", "shirt1"),
+        Product("Deveslopes Badge light Grey","$32.49", "shirt2"),
+        Product("Deveslopes logo Shirt Red","$29.99", "shirt3"),
+        Product("Deveslopes Hustle","$14.99", "shirt4"),
+        Product("Kikflip Studios","$34.99", "shirt5"),
         Product("Deveslopes Shirt Black","$29.49", "shirt1"),
         Product("Deveslopes Badge light Grey","$32.49", "shirt2"),
         Product("Deveslopes logo Shirt Red","$29.99", "shirt3"),
